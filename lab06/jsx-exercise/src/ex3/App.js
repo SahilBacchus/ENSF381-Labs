976c934f217ff913920171af1cddf45d69c65cb4
Sahil Bacchus <sahilbacchus@gmail.com>
2025-03-07 23:56:34 -0700

    made ex3 folder

---
+++ lab06/jsx-exercise/src/ex3/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import EngineeringTopics from './EngineeringTopics.js';
+
+function App() {
+  return (
+    <div>
+    <EngineeringTopics/>
+    </div>
+  );
+}
+
+export default App;
