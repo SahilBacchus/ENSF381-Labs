9a8952471244d513df695e9d7c64912cbae463e5
Sahil Bacchus <sahilbacchus@gmail.com>
2025-03-29 00:09:57 -0600

    uploaded the login.js

--- lab09/house-brice-breticter/src/Login.js
+++ lab09/house-brice-breticter/src/Login.js
@@ -1,14 +1,49 @@
+import React, { useState } from 'react';
+import {useNavigate } from 'react-router-dom';
+import './Login.css'
 
+const navigate = useNavigate();
 
-function Login() {
-    return (
-      <div className="Login">
-        <label for="fname">First name:</label><br/>
-        <input type="text" id="fname" name="fname"/><br/>
-        <label for="lname">Last name:</label><br/>
-        <input type="text" id="lname" name="lname"/>
+function AuthenticationForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  function handleAuthentication() {
+    fetch('http://127.0.0.1:5000/validate_login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({'username':username, 'password':password}),
+    })
+    .then(response => {
+      if (response.ok) {
+        navigate("/predict")
+      } else {
+        throw new Error('Authentication failed');
+      }
+    })
+    .then(data => setMessage(data.message))
+    .catch(error => setMessage('Authentication failed. Incorrect username or password.'));};
+
+  return (
+    <div class="login_form">
+      <label>
+        Username:
+        </label>
+          <input type="text" onChange={(e) => setUsername(e.target.value)} />
+          <br />
+          <label>
+            Password:
+          </label>
+            <input type="password" onChange={(e) => setPassword(e.target.value)} />
+          <br />
+          <button onClick={handleAuthentication}>Submit</button>
+          <br />
+          <p>{message}</p>
       </div>
-    );
-  }
-  
-  export default Login;
+
+
+      );
+    };
+export default AuthenticationForm;
